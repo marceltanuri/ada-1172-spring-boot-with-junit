5d17e2f0991929f82bd1643bf8b803e882dcadc8
Iago Teles <iagooteles@gmail.com>
2024-11-22 21:26:30 -0300

    implemented ContatoService

--- src/main/java/br/com/ada/t1172/agenda_contatots/service/ContatoService.java
+++ src/main/java/br/com/ada/t1172/agenda_contatots/service/ContatoService.java
@@ -5,6 +5,7 @@ import br.com.ada.t1172.agenda_contatots.repository.ContatosRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContatoService {
@@ -16,17 +17,38 @@ public class ContatoService {
     }
 
     public Contato salvarContato(Contato contato) {
-        return null;
+        if (contato.getNome() == null || contato.getNome().trim().isEmpty()) {
+            throw new IllegalArgumentException("O nome do contato é obrigatório.");
+        }
+
+        Optional<Contato> contatoComEmailExistente = contatosRepository.findByEmail(contato.getEmail());
+
+        if (contatoComEmailExistente.isPresent()) {
+            throw new IllegalArgumentException("Já existe um contato com este e-mail.");
+        }
+
+        if (contato.getEmail() == null || !contato.getEmail().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+            throw new IllegalArgumentException("O e-mail do contato é obrigatório e deve ser válido.");
+        }
+
+        return contatosRepository.save(contato);
     }
 
     public Contato buscarContatoPorId(long l) {
-        return null;
+        Contato contato = contatosRepository.findById(l)
+                .orElseThrow(() -> new RuntimeException("Contato: " + l + " - não encontrado."));
+
+        return contato;
     }
 
     public Contato buscarContatoPorEmail(String mail) {
-        return null;
+        Contato contato = contatosRepository.findByEmail(mail)
+                .orElseThrow(() -> new RuntimeException("Contato: " + mail + " - não encontrado."));
+
+        return contato;
     }
 
     public void excluirContato(long l) {
+        contatosRepository.deleteById(l);
     }
 }
