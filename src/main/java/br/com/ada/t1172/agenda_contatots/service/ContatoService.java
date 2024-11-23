bdc9b15337520dcdb306b7762213e4356428d1c6
Marcel Tanuri <marcel.tanuri@gmail.com>
2024-11-22 21:34:17 -0300

    add service implementation

--- src/main/java/br/com/ada/t1172/agenda_contatots/service/ContatoService.java
+++ src/main/java/br/com/ada/t1172/agenda_contatots/service/ContatoService.java
@@ -5,6 +5,7 @@ import br.com.ada.t1172.agenda_contatots.repository.ContatosRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ContatoService {
@@ -16,17 +17,33 @@ public class ContatoService {
     }
 
     public Contato salvarContato(Contato contato) {
-        return null;
+        if(contatosRepository.findByEmail(contato.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Já existe um contato com este e-mail.");
+        }
+
+        if(Objects.isNull(contato.getNome()) || contato.getNome().isEmpty())
+            throw new IllegalArgumentException("O nome do contato é obrigatório.");
+
+        if(!isValidEmail(contato.getEmail()))
+            throw new IllegalArgumentException("O e-mail do contato é obrigatório e deve ser válido.");
+
+        return contatosRepository.save(contato);
     }
 
-    public Contato buscarContatoPorId(long l) {
-        return null;
+    public Contato buscarContatoPorId(long id) {
+        return contatosRepository.findById(id).get();
     }
 
     public Contato buscarContatoPorEmail(String mail) {
-        return null;
+        return contatosRepository.findByEmail(mail).get();
     }
 
-    public void excluirContato(long l) {
+    public void excluirContato(long id) {
+        contatosRepository.deleteById(id);
     }
+
+    private boolean isValidEmail(String email) {
+        return email != null && email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+    }
+
 }
